0a468eff4c1857351cf1e27bc060fc16ebf32576
Isaac <isaaco2002@hotmail.com>
2022-03-27 12:11:27 +1100

    Simple Movement Calculations
    Finds difference between current location and where to move to

---
+++ MCU/moveto.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+double newaz;
+double newalt;
+double oldaz;
+double oldalt;
+double azchange;
+double altchange;
+
+int main(void){
+    altchange=newalt-oldalt;
+    azchange=newaz-oldaz;
+}
