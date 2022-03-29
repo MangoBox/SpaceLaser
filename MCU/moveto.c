8261c90ea21ba2063593700219c3d9f4c09bfbbe
Nymeria44 <100979539+Nymeria44@users.noreply.github.com>
2022-03-29 17:32:16 +1100

    Update moveto.c

--- MCU/moveto.c
+++ MCU/moveto.c
@@ -1,12 +1,37 @@
 #include<stdio.h>
-double newaz;
-double newalt;
-double oldaz;
-double oldalt;
-double azchange;
-double altchange;
+
+struct horizon_co {
+double az;
+double alt;
+};
+
+/*****************************************************************
+GLOBAL VARIABLES:
+*****************************************************************/
+
+/*****************************************************************
+FUNCTION DECLARATION
+*****************************************************************/
+/*Function to calculate delta co*/
+struct horizon_co calulate_delta (horizon_co, horizon_co);
+
 
 int main(void){
-    altchange=newalt-oldalt;
-    azchange=newaz-oldaz;
+    /*Local variables*/
+    struct horizon_co recieved_co [30], current_co, delta_co ;
+    
+    delta_co = calulate_delta (current_co, recieved_co[0]);
 }
+
+/*****************************************************************
+FUNCTIONS
+*****************************************************************/
+
+struct horizon_co calulate_delta(horizon_co current_co, horizon_co future_co){
+/*Local variable*/
+horizon_co delta_co;
+/*Function*/
+delta_co.alt = future_co.alt - current_co.alt;
+delta_co.az = future_co.az - current_co.az;
+return delta_co;
+};
