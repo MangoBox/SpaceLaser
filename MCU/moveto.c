4682526956b720570a10d3f280e366c1993198d7
Nymeria44 <100979539+Nymeria44@users.noreply.github.com>
2022-03-31 16:46:52 +1100

    Update moveto.c

--- MCU/moveto.c
+++ MCU/moveto.c
@@ -8,9 +8,18 @@ double alt;
 /*****************************************************************
 GLOBAL VARIABLES:
 *****************************************************************/
+/*Number of data point we are reciving*/
+int NUM_DATAPT = 50;
 
 /*****************************************************************
-FUNCTION DECLARATION
+FUNCTION DECLARATION:
+Caclculating Acceleration
+Calc Deceleration?
+Current velocity
+Communication x 7
+Calc Current pos
+Defining path
+Step calc (how do you move motors)
 *****************************************************************/
 /*Function to calculate delta co*/
 horizon_co calulate_delta (horizon_co, horizon_co);
@@ -18,7 +27,7 @@ horizon_co calulate_delta (horizon_co, horizon_co);
 
 int main(void){
     /*Local variables*/
-    struct horizon_co recieved_co [30], current_co, delta_co ;
+    struct horizon_co recieved_co [NUM_DATAPT], current_co, delta_co ;
     
     delta_co = calulate_delta (current_co, recieved_co[0]);
 }
@@ -28,10 +37,10 @@ FUNCTIONS
 *****************************************************************/
 
 horizon_co calulate_delta(horizon_co current_co, horizon_co future_co){
-/*Local variable*/
-horizon_co delta_co;
-/*Function*/
-delta_co.alt = future_co.alt - current_co.alt;
-delta_co.az = future_co.az - current_co.az;
-return delta_co;
+    /*Local variable*/
+    horizon_co delta_co;
+    /*Function*/
+    delta_co.alt = future_co.alt - current_co.alt;
+    delta_co.az = future_co.az - current_co.az;
+    return delta_co;
 };
