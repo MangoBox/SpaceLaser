23996ee33f57e103878374e0b9004ac1cf48c3d9
Jacob Solsky <jacobsolsky@gmail.com>
2022-04-21 14:59:31 +1000

    Testing horizon api and re

--- Horizons/horizons.py
+++ Horizons/horizons.py
@@ -1,63 +1,42 @@
 import sys
-import json
 import base64
 import requests
+import pandas as pd
+import re
+from pathlib import Path
 
-# Define API URL and SPK filename:
+
+
+queries_filepath = Path('/Users/jacobsolsky/Documents/GitHub/SpaceLaser/Horizons/Horizons_queries.csv') 
+
+queries = pd.read_csv(queries_filepath)
+print(queries)
+
 url = 'https://ssd.jpl.nasa.gov/api/horizons_file.api'
 spk_filename = 'spk_file.bsp'
 file = 'documents/GitHib/SpaceLaser/input.txt'
 
-# Get the Horizons API input file from the command-line:
-try:
-    f = open(file)
-except OSError as err:
-  print("Unable to open input file '{0}': {1}".format(file, err))
+exampel = "https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'"
 
-# Build and submit the API request and decode the JSON-response:
-response = requests.post(url, data={'format':'json'}, files={'input': f})
-f.close()
-try:
-  data = json.loads(response.text)
-except ValueError:
-  print("Unable to decode JSON results")
+request_base = "https://ssd.jpl.nasa.gov/api/horizons.api?format=text"
 
-# If the request was valid...
-if (response.status_code == 200):
-  #
-  # If the SPK file was generated, decode it and write it to the output file:
-  if "spk" in data:
-    #
-    # If a suggested SPK file basename was provided, use it:
-    if "spk_file_id" in data:
-      spk_filename = data["spk_file_id"] + ".bsp"
-    try:
-      f = open(spk_filename, "wb")
-    except OSError as err:
-      print("Unable to open SPK file '{0}': {1}".format(spk_filename, err))
-    #
-    # Decode and write the binary SPK file content:
-    f.write(base64.b64decode(data["spk"]))
+pattern = "\$\$SOE\n([\s\S]+)\n\$\$EOE"
+
+
+#for i in range(len(queries.index)):
+#query_df = queries.iloc[i]
+
+query_link = request_base
+#query_link += "&COMMAND='" + query_df["COMMAND"]+"'"
+query_link += "&COMMAND='" + "499"+"'"
+response = requests.get(query_link)
+text = (response.text)
+
+with open('/Users/jacobsolsky/Documents/GitHub/SpaceLaser/Horizons/temp.txt', 'w') as f:
+    f.write(text)
     f.close()
-    print("wrote SPK content to {0}".format(spk_filename))
-    sys.exit()
-  #
-  # Otherwise, the SPK file was not generated so output an error:
-  print("ERROR: SPK file not generated")
-  if "result" in data:
-    print(data["result"])
-  else:
-    print(response.text)
-  sys.exit(1)
 
-# If the request was invalid, extract error content and display it:
-if (response.status_code == 400):
-  data = json.loads(response.text)
-  if "message" in data:
-    print("MESSAGE: {}".format(data["message"]))
-  else:
-    print(json.dumps(data, indent=2))
+elements = re.match(pattern, text)
+print(elements)
 
-# Otherwise, some other error occurred:
-print("response code: {0}".format(response.status_code))
-sys.exit(2)
+#elements = elements.removeprefix("$$SOE/n").removesuffix("$$EOE/n")
