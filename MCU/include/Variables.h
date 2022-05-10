b575591d2dd7c4fca559a1c4d53074fc4ac355fe
Hugh <hugh.hannan@student.uts.edu.au>
2022-05-10 17:49:09 +1000

    Communication changes for MCU code

--- MCU/include/Variables.h
+++ MCU/include/Variables.h
@@ -2,12 +2,13 @@
 GLOBAL VARIABLES:
 *****************************************************************/
 //Number of data points we are reciving
-int NUM_DATAPT = 20;
+int NUM_DATAPT = 1;
 // Time interval between data points in ms
 int T_interval = 6000;  //Hard coded for 5 minutes
 
 //Print String_Strtok function outputs??
 const bool Strtok_outputs = false;
+const bool Using_hardcode = false;
 
 //Main data type
 struct horizon_co {
--- MCU/src/main.cpp
+++ MCU/src/main.cpp
@@ -4,13 +4,16 @@
 #include<string.h>
 #include <iostream>
 #include <cstdlib>
+//Motor thingo 
+#include <AccelStepper.h>
 //Our header files
 #include <Variables.h>
 #include <Functions.h>
 
+
 void setup(){
     // Local variables
-    int wait_period = 306000;
+    int wait_period = 306000; // 5 mins
     int baud_rate = 9600;
 
     //Starting serial
@@ -26,15 +29,16 @@ void loop(){
      * Recieving Data from computer
     ****************************************************/
     // START WITH A WHILE AND THEN MOVE TO IF MAYBE
+    //timer interput EXAMPLE IS BLINK WITHOUT DELAY SERIAL WITHOUT WAITING
+    // LOOK UP SERIAL INERUPT
     while(Serial.available() == 0){
     }
     
     // Recieving number of data sets being sent and time interval (ms)
-    NUM_DATAPT = Serial.readStringUntil (',').toInt();
-    T_interval = Serial.readStringUntil ('\r').toInt();
+    //NUM_DATAPT = Serial.readStringUntil (',').toInt();
+    //T_interval = Serial.readStringUntil ('\r').toInt();
 
     //ERROR CHECK USING REGEX. First save as string and pattern match.
-
 
     //Delcaring main data storage variable
     struct horizon_co recieved_co [NUM_DATAPT];
@@ -44,39 +48,47 @@ void loop(){
     char Raw_Data [55];
     for (int i = 0; i < NUM_DATAPT; i ++){
         Raw_Data_S = Serial.readStringUntil('\r');
+        //delay (30);
+        Serial.print ("raw data = ");
+        Serial.println (Raw_Data_S);
         // Do a REGEX check here
         Raw_Data_S.toCharArray(Raw_Data, 55);
-        recieved_co[i] = String_strtok (Raw_Data, Strtok_outputs);
+        recieved_co[i] = String_strtok (Raw_Data, Strtok_outputs);  
+        Serial.print ("az / alt = ");
+        Serial.print (recieved_co[i].az);
+        Serial.print(", ");
+        Serial.println(recieved_co[i].alt);
     }
-
     /****************************************************
      * TESTING WITH HARD CODED DATA
     ****************************************************/
-    char Raw_Data_1[55] = "133.193231,-27.095697,2022-04-29,22:07:06";
-    char Raw_Data_2[55] = "180.123433,-95.900123,2022-04-29,22:07:11";
+    if (Using_hardcode){
+        char Raw_Data_1[55] = "133.193231,-27.095697,2022-04-29,22:07:06";
+        char Raw_Data_2[55] = "180.123433,-95.900123,2022-04-29,22:07:11";
 
-    //Extracting Data Strings from raw data
-    Serial.print ("Raw data string 1 = ");
-    Serial.println (Raw_Data_1);
-    
-    recieved_co[1] = String_strtok (Raw_Data_1, Strtok_outputs);
-    Serial.println (recieved_co[1].alt);
-    Serial.println (recieved_co[1].az);
-    delay (5000);
+        //Extracting Data Strings from raw data
+        Serial.print ("Raw data string 1 = ");
+        Serial.println (Raw_Data_1);
+        
+        recieved_co[1] = String_strtok (Raw_Data_1, Strtok_outputs);
+        Serial.println (recieved_co[1].alt);
+        Serial.println (recieved_co[1].az);
+        delay (5000);
 
-    Serial.print ("Raw data string 2 = ");
-    Serial.println (Raw_Data_2);
-    
-    recieved_co[2] = String_strtok (Raw_Data_2, Strtok_outputs);;
-    Serial.println (recieved_co[2].alt);
-    Serial.println (recieved_co[2].az);
-    delay (5000);
+        Serial.print ("Raw data string 2 = ");
+        Serial.println (Raw_Data_2);
+        
+        recieved_co[2] = String_strtok (Raw_Data_2, Strtok_outputs);;
+        Serial.println (recieved_co[2].alt);
+        Serial.println (recieved_co[2].az);
+        delay (5000);
 
-    //Calulating the speed of az and alt (deg/ms)
-    delta_co = calulate_delta (recieved_co[1], recieved_co[2]);
-    Serial.print ("delta az = ");
-    Serial.println (delta_co.az, 10);
-    Serial.print ("delta alt = ");
-    Serial.println (delta_co.alt, 10);
-    delay (5000);
+        //Calulating the speed of az and alt (deg/ms)
+        delta_co = calulate_delta (recieved_co[1], recieved_co[2]);
+        Serial.print ("delta az = ");
+        Serial.println (delta_co.az, 10);
+        Serial.print ("delta alt = ");
+        Serial.println (delta_co.alt, 10);
+        delay (5000);
+    }
 }
